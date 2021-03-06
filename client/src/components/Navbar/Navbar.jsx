faecd91cd17c8b893b13ce4b04edb35ddc6162d0
Dheeraj Gandhi <gandhidheeraj9@gmail.com>
2021-03-06 20:10:31 +0530

    changes

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -7,58 +7,58 @@ import { useSelector, useDispatch } from "react-redux";
 import { selectUser, removeUser } from "../../features/userSlice";
 
 const Navbar = () => {
-  const user = useSelector(selectUser);
-  const dispatch = useDispatch();
-  const history = useHistory();
-  const logoutuser = () => {
-    dispatch(removeUser());
-    localStorage.removeItem("connect2dot0");
-    history.push("/login");
-  };
+	const user = useSelector(selectUser);
+	const dispatch = useDispatch();
+	const history = useHistory();
+	const logoutuser = () => {
+		dispatch(removeUser());
+		localStorage.removeItem("connect2dot0");
+		history.push("/login");
+	};
 
-  return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <div className={styles.row}>
-          <div className={styles.logo}>
-            <Link to="/" href="#">
-              Connects
-            </Link>
-          </div>
-          <input type="checkbox" className={styles.nav_check} />
-          <label htmlFor="nav-check" className={styles.nav_toggler}>
-            <span></span>
-          </label>
-          <nav className={styles.nav}>
-            <div className={styles.g1}>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/donate">Donate</Link>
-                </li>
-                <li>
-                  <Link to="/locate">Locate</Link>
-                </li>
-                <li>
-                  <Link to="/ngos">NGO's</Link>
-                </li>
-              </ul>
-            </div>
-            <div className={styles.g2}>
-              {user && <Link to="/dashboard">Dashboard</Link>}
-              {user ? (
-                <Button title="Logout" onclick={logoutuser} />
-              ) : (
-                <Button title="Login" onclick={() => history.push("/login")} />
-              )}
-            </div>
-          </nav>
-        </div>
-      </div>
-    </header>
-  );
+	return (
+		<header className={styles.header}>
+			<div className={styles.container}>
+				<div className={styles.row}>
+					<div className={styles.logo}>
+						<Link to="/" href="#">
+							Connect It
+						</Link>
+					</div>
+					<input type="checkbox" className={styles.nav_check} />
+					<label htmlFor="nav-check" className={styles.nav_toggler}>
+						<span></span>
+					</label>
+					<nav className={styles.nav}>
+						<div className={styles.g1}>
+							<ul>
+								<li>
+									<Link to="/">Home</Link>
+								</li>
+								<li>
+									<Link to="/donate">Donate</Link>
+								</li>
+								<li>
+									<Link to="/locate">Locate</Link>
+								</li>
+								<li>
+									<Link to="/ngos">NGO's</Link>
+								</li>
+							</ul>
+						</div>
+						<div className={styles.g2}>
+							{user && <Link to="/dashboard">Dashboard</Link>}
+							{user ? (
+								<Button title="Logout" onclick={logoutuser} />
+							) : (
+								<Button title="Login" onclick={() => history.push("/login")} />
+							)}
+						</div>
+					</nav>
+				</div>
+			</div>
+		</header>
+	);
 };
 
 export default Navbar;
