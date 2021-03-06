f3a223aaafba5ff72c4e149865bf70264d9755c8
Riddhi Siddarkar <riddhisiddarkar@gmail.com>
2021-03-06 18:08:01 +0530

    adding the all ngos page

--- client/src/components/AllNGOs/AllNGOs.jsx
+++ client/src/components/AllNGOs/AllNGOs.jsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import styles from "./AllNGOs.module.css";
 import EachNGO from "./EachNGO/EachNGO";
+import axios from "axios";
 
 const AllNGOs = () => {
-	return (
-		<div className={styles.allngospage}>
-			<h2 className={styles.allngospage_heading}>
-				<span>NGO's</span> registered with us
-			</h2>
-			<div className={styles.allngospage_container}>
-				<EachNGO />
-				<EachNGO />
-				<EachNGO />
-				<EachNGO />
-			</div>
-		</div>
-	);
+  const [Ngos, setNgos] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/ngo/all")
+      .then((res) => {
+        setNgos(res.data);
+      })
+      .catch((error) => {
+        console.log("There is an error in getting the ngo");
+        console.log(error);
+      });
+  }, []);
+  return (
+    <div className={styles.allngospage}>
+      <h2 className={styles.allngospage_heading}>
+        <span>NGO's</span> registered with us
+      </h2>
+      <div className={styles.allngospage_container}>
+        {Ngos.map((ele, i) => (
+          <EachNGO data={ele} key={i} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllNGOs;
--- client/src/components/AllNGOs/EachNGO/EachNGO.jsx
+++ client/src/components/AllNGOs/EachNGO/EachNGO.jsx
@@ -4,30 +4,34 @@ import styles from "./EachNGO.module.css";
 import image from "../../../assets/images/childImage.png";
 import website_logo from "../../../assets/icons/website.svg";
 
-const EachNGO = () => {
-	return (
-		<div className={styles.eachngo}>
-			<img src={image} alt="ngo" className={styles.eachngo_image} />
-			<p className={styles.eachngo_name}>Batata matata foundation</p>
-			<p className={styles.eachngo_location}>India ,Goa, ponda,</p>
-			<div className={styles.info}>
-				<p className={styles.eachngo_type}>Type: Oldagehome</p>
-				<p className={styles.eachngo_email}>Email: gandhidheeraj0@gmail.com</p>
-				<p className={styles.eachngo_phoneno}>Ph no: 0832474833</p>
-			</div>
-
-			<div className={styles.eachngo_websitediv}>
-				<img
-					src={website_logo}
-					alt="website"
-					className={styles.eachngo_websitediv_image}
-				/>
-				<p className={styles.eachngo_websitediv_website}>
-					www.tejasimangale.mamuti.com
-				</p>
-			</div>
-		</div>
-	);
+const EachNGO = ({ data }) => {
+  console.log(data);
+  return (
+    <div className={styles.eachngo}>
+      <img src={image} alt="ngo" className={styles.eachngo_image} />
+      <p className={styles.eachngo_name}>{data.name}</p>
+      <p className={styles.eachngo_location}>{data.location}</p>
+      <div className={styles.info}>
+        <p className={styles.eachngo_type}>
+          Type: {data.category[0].old && "Old age home, "}
+          {data.category[0].children && "Orphange, "}
+          {data.category[0].others && "Others, "}
+        </p>
+        <p className={styles.eachngo_email}>Email: {data.email}</p>
+        <p className={styles.eachngo_phoneno}>Ph no: {data.phoneNo}</p>
+      </div>
+      {data.website && (
+        <div className={styles.eachngo_websitediv}>
+          <img
+            src={website_logo}
+            alt="website"
+            className={styles.eachngo_websitediv_image}
+          />
+          <p className={styles.eachngo_websitediv_website}>{data.website}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default EachNGO;
