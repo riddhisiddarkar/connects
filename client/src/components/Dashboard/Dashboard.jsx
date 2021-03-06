ba4808afdf3afa9bf20fdd691c00e344331d19cc
Dheeraj Gandhi <gandhidheeraj9@gmail.com>
2021-03-06 18:42:14 +0530

    dashboard

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -5,27 +5,40 @@ import Button from "../../UI/Button/Button";
 import EachDonation from "./EachDonation/EachDonation";
 
 const Dashboard = () => {
-  return (
-    <div className={styles.donationpage}>
-      <p className={styles.donation_text}>Donations</p>
-      <div className={styles.donationpage_details}>
-        <img className={styles.donationpage.ngoimg} src="" />
-        <p className={styles.donationpage_ngoname}>BATATA MATATA FOUNDATION</p>
-        <p className={styles.donationpage_location}>
-          Location: India, Goa , Ponda
-        </p>
-        <p className={styles.donationpage_contact}>Contact: 9325995634</p>
-        <p className={styles.donationpage_email}>
-          Email: gandhidheeraj9@gmail.com
-        </p>
-        <p className={styles.donationpage_type}>Type: Orphanage</p>
-        <Button title="View Website" />
-      </div>
-      <div className={styles.donations}>
-        <EachDonation />
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.donationpage}>
+			<div className={styles.donationpage_details}>
+				<img
+					className={styles.donationpage_ngoimg}
+					src="https://upvey.com/wp-content/uploads/2020/04/NGO-Hindi.jpg"
+				/>
+				<p className={styles.donationpage_ngoname}>BATATA MATATA FOUNDATION</p>
+
+				<div className={styles.other}>
+					<p className={styles.donationpage_location}>
+						Location: India, Goa , Ponda
+					</p>
+					<p className={styles.donationpage_contact}>Contact: 9325995634</p>
+					<p className={styles.donationpage_email}>
+						Email: gandhidheeraj9@gmail.com
+					</p>
+					<p className={styles.donationpage_type}>Type: Orphanage</p>
+				</div>
+				<Button title="View Website" />
+			</div>
+			<div className={styles.donations}>
+				<p className={styles.donation_text}>Donations</p>
+				<div className={styles.bckgnd}></div>
+				<EachDonation />
+				<EachDonation />
+
+				<div className={styles.totalamt}>
+					<p>Total Amount</p>
+					<div className={styles.value}>4593</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Dashboard;
--- client/src/components/Dashboard/EachDonation/EachDonation.jsx
+++ client/src/components/Dashboard/EachDonation/EachDonation.jsx
@@ -3,15 +3,15 @@ import React from "react";
 import styles from "./EachDonation.module.css";
 
 const EachDonation = () => {
-  return (
-    <div className={styles.donation}>
-      <div className={styles.donationdetails}>
-        <p className={styles.donationdetails_name}>Gandhi</p>
-        <p className={styles.donationdetails_no}>09876543</p>
-      </div>
-      <p className={styles.donationdetails_amt}>7655</p>
-    </div>
-  );
+	return (
+		<div className={styles.donation}>
+			<div className={styles.donationdetails}>
+				<p className={styles.donationdetails_name}>Dheeraj Gandhi</p>
+				<p className={styles.donationdetails_no}>09876543</p>
+			</div>
+			<p className={styles.donationdetails_amt}>8655</p>
+		</div>
+	);
 };
 
 export default EachDonation;
