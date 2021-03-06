7a4895bf1df66358ad16176b326b62e2c0629589
Dheeraj Gandhi <gandhidheeraj9@gmail.com>
2021-03-06 19:38:26 +0530

    added

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -8,73 +8,36 @@ import EachDonation from "./EachDonation/EachDonation";
 import { selectUser } from "../../features/userSlice";
 
 const Dashboard = () => {
-<<<<<<< HEAD
+	const history = useHistory();
+	const user = useSelector(selectUser);
+
+	useEffect(() => {
+		if (!user) {
+			history.push("/login");
+		}
+	}, []);
+
 	return (
 		<div className={styles.donationpage}>
+			<p className={styles.donation_text}>Donations</p>
 			<div className={styles.donationpage_details}>
-				<img
-					className={styles.donationpage_ngoimg}
-					src="https://upvey.com/wp-content/uploads/2020/04/NGO-Hindi.jpg"
-				/>
+				<img className={styles.donationpage_ngoimg} src="" />
 				<p className={styles.donationpage_ngoname}>BATATA MATATA FOUNDATION</p>
-
-				<div className={styles.other}>
-					<p className={styles.donationpage_location}>
-						Location: India, Goa , Ponda
-					</p>
-					<p className={styles.donationpage_contact}>Contact: 9325995634</p>
-					<p className={styles.donationpage_email}>
-						Email: gandhidheeraj9@gmail.com
-					</p>
-					<p className={styles.donationpage_type}>Type: Orphanage</p>
-				</div>
+				<p className={styles.donationpage_location}>
+					Location: India, Goa , Ponda
+				</p>
+				<p className={styles.donationpage_contact}>Contact: 9325995634</p>
+				<p className={styles.donationpage_email}>
+					Email: gandhidheeraj9@gmail.com
+				</p>
+				<p className={styles.donationpage_type}>Type: Orphanage</p>
 				<Button title="View Website" />
 			</div>
 			<div className={styles.donations}>
-				<p className={styles.donation_text}>Donations</p>
-				<div className={styles.bckgnd}></div>
 				<EachDonation />
-				<EachDonation />
-
-				<div className={styles.totalamt}>
-					<p>Total Amount</p>
-					<div className={styles.value}>4593</div>
-				</div>
 			</div>
 		</div>
 	);
-=======
-  const history = useHistory();
-  const user = useSelector(selectUser);
-
-  useEffect(() => {
-    if (!user) {
-      history.push("/login");
-    }
-  }, []);
-
-  return (
-    <div className={styles.donationpage}>
-      <p className={styles.donation_text}>Donations</p>
-      <div className={styles.donationpage_details}>
-        <img className={styles.donationpage_ngoimg} src="" />
-        <p className={styles.donationpage_ngoname}>BATATA MATATA FOUNDATION</p>
-        <p className={styles.donationpage_location}>
-          Location: India, Goa , Ponda
-        </p>
-        <p className={styles.donationpage_contact}>Contact: 9325995634</p>
-        <p className={styles.donationpage_email}>
-          Email: gandhidheeraj9@gmail.com
-        </p>
-        <p className={styles.donationpage_type}>Type: Orphanage</p>
-        <Button title="View Website" />
-      </div>
-      <div className={styles.donations}>
-        <EachDonation />
-      </div>
-    </div>
-  );
->>>>>>> 519caf5624fa9d9a65522feaa4d69f0f3ca0f032
 };
 
 export default Dashboard;
