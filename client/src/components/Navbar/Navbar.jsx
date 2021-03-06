345c04b2ad3e3a33f7628d51426d4405c7d472c3
Dheeraj Gandhi <gandhidheeraj9@gmail.com>
2021-03-06 17:28:39 +0530

    added css

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -5,46 +5,46 @@ import styles from "./Navbar.module.css";
 import { useHistory, Link } from "react-router-dom";
 
 const Navbar = () => {
-  const history = useHistory();
-  return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <div className={styles.row}>
-          <div className={styles.logo}>
-            <Link to="/" href="#">
-              Logo
-            </Link>
-          </div>
-          <input type="checkbox" className={styles.nav_check} />
-          <label for="nav-check" className={styles.nav_toggler}>
-            <span></span>
-          </label>
-          <nav className={styles.nav}>
-            <div className={styles.g1}>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/donate">Donate</Link>
-                </li>
-                <li>
-                  <Link to="/locate">Locate</Link>
-                </li>
-                <li>
-                  <Link to="/ngos">NGO's</Link>
-                </li>
-              </ul>
-            </div>
-            <div className={styles.g2}>
-              <Link to="/dashboard">Dashboard</Link>
-              <Button title="Login" onclick={() => history.push("/login")} />
-            </div>
-          </nav>
-        </div>
-      </div>
-    </header>
-  );
+	const history = useHistory();
+	return (
+		<header className={styles.header}>
+			<div className={styles.container}>
+				<div className={styles.row}>
+					<div className={styles.logo}>
+						<Link to="/" href="#">
+							Connect it
+						</Link>
+					</div>
+					<input type="checkbox" className={styles.nav_check} />
+					<label for="nav-check" className={styles.nav_toggler}>
+						<span></span>
+					</label>
+					<nav className={styles.nav}>
+						<div className={styles.g1}>
+							<ul>
+								<li>
+									<Link to="/">Home</Link>
+								</li>
+								<li>
+									<Link to="/donate">Donate</Link>
+								</li>
+								<li>
+									<Link to="/locate">Locate</Link>
+								</li>
+								<li>
+									<Link to="/ngos">NGO's</Link>
+								</li>
+							</ul>
+						</div>
+						<div className={styles.g2}>
+							<Link to="/dashboard">Dashboard</Link>
+							<Button title="Login" onclick={() => history.push("/login")} />
+						</div>
+					</nav>
+				</div>
+			</div>
+		</header>
+	);
 };
 
 export default Navbar;
